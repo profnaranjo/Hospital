7db7507e8c61e46b73ef50d251e736920c34a713
prof-naranjo <73539896+prof-naranjo@users.noreply.github.com>
2020-11-14 11:46:48 -0500

    Changed db cpnnection to cloud cluster

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,6 +7,8 @@ mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
 
 //Changed by Eduardo Naranjo
-mongoose.connect('mongodb://localhost:27017/nhs-app');
+//mongoose.connect('mongodb://localhost:27017/nhs-app');
+
+mongoose.connect('mongodb+srv://user1:<password>@cluster0.rhhqn.mongodb.net/<dbname>?retryWrites=true&w=majority');
 
 module.exports = {mongoose};
